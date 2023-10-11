ee38ad6bd93e5f003ac3f47d0a783232c7ca230a
sando96ale <sandoval.alexis.emanuel@gmail.com>
2023-10-11 19:56:55 -0300

    Seguimos avanzando

--- pedido_hamburguesa.py
+++ pedido_hamburguesa.py
@@ -5,6 +5,9 @@ def mostrar_menu(menu):
         print(f'{i}. {comida}: {precio}')
         
 def agregar_carrito(opcion, eleccion_menu):
+    if opcion == len(eleccion_menu):
+        return
+    
     menu_comida = list(eleccion_menu.keys())
     eleccion_comida = menu_comida[opcion - 1]
     carrito_de_compras.update({eleccion_comida: eleccion_menu[eleccion_comida]})
@@ -15,84 +18,134 @@ print('Bienvenido a nuestro servicio de comida rápida')
 print('Ofrecemos una amplia variedad de opciones deliciosas y rápidas para satisfacer tu apetito.')
 print('¡Esperamos que disfrutes de tu comida con nosotros!')
 print('Aquí puedes ver todo lo que tenemos para ofrecerte')
+print()
 
-while True:
-    
+eleccion = True
+while eleccion:
     mostrar_menu(menu)
+    print()
     opcion = input('Seleccione una opcion: ')
+    print()
     
     if opcion == '1':
-        print('A elegido hamburguesas')
+        print('Ha elegido la sección de Hamburguesas')
+        print('A continuación, nuestras opciones disponibles:')
+        print()
         mostrar_menu(hamburguesa)
+        print()
         opcion1 = int(input('Seleccione una opcion: '))
+        print()
         agregar_carrito(opcion1, hamburguesa)
         print('Su pedido se ha agregado al carrito')
         
     elif opcion == '2':
-        print('A elegido Sandwich de pollo')
+        print('Ha elegido la sección de Sandwich de pollo')
+        print('A continuación, nuestras opciones disponibles:')
+        print()
         mostrar_menu(sandwich_pollo)
+        print()
         opcion1 = int(input('Seleccione una opcion: '))
+        print()
         agregar_carrito(opcion1, sandwich_pollo)
         print('Su pedido se ha agregado al carrito')
         
     elif opcion == '3':
-        print('A elegido Papas y complementos')
+        print('Ha elegido la sección de Papas y complementos')
+        print('A continuación, nuestras opciones disponibles:')
+        print()
         mostrar_menu(papas_complementos)
+        print()
         opcion1 = int(input('Seleccione una opcion: '))
+        print()
         agregar_carrito(opcion1, papas_complementos)
         print('Su pedido se ha agregado al carrito')
         
     elif opcion == '4':
-        print('A elegido Bebidas')
+        print('Ha elegido la sección de Bebidas')
+        print('A continuación, nuestras opciones disponibles:')
+        print()
         mostrar_menu(bebidas)
+        print()
         opcion1 = int(input('Seleccione una opcion: '))
+        print()
         agregar_carrito(opcion1, bebidas)
         print('Su pedido se ha agregado al carrito')
         
     elif opcion == '5':
-        print('A elegido Postres')
+        print('Ha elegido la sección de Postres')
+        print('A continuación, nuestras opciones disponibles:')
+        print()
         mostrar_menu(postres)
+        print()
         opcion1 = int(input('Seleccione una opcion: '))
+        print()
         agregar_carrito(opcion1, postres)
         print('Su pedido se ha agregado al carrito')
         
     elif opcion == '6':
-        print('A elegido Ensaladas')
+        print('Ha elegido la sección de Ensaladas')
+        print('A continuación, nuestras opciones disponibles:')
+        print()
         mostrar_menu(ensaladas)
+        print()
         opcion1 = int(input('Seleccione una opcion: '))
+        print()
         agregar_carrito(opcion1, ensaladas)
         print('Su pedido se ha agregado al carrito')
         
     elif opcion == '7':
-        print('A elegido Mc Café')
+        print('Ha elegido la sección de Mc Café')
+        print('A continuación, nuestras opciones disponibles:')
+        print()
         mostrar_menu(mc_café)
+        print()
         opcion1 = int(input('Seleccione una opcion: '))
+        print()
         agregar_carrito(opcion1, mc_café)
         print('Su pedido se ha agregado al carrito')
         
     elif opcion == '8':
-        print('A elegido Cajita Feliz')
+        print('Ha elegido la sección de Cajita Feliz')
+        print('A continuación, nuestras opciones disponibles:')
+        print()
         mostrar_menu(cajita_feliz)
+        print()
         opcion1 = int(input('Seleccione una opcion: '))
+        print()
         agregar_carrito(opcion1, cajita_feliz)
         print('Su pedido se ha agregado al carrito')
         
     elif opcion == '9':
-        print('A elegido Armar tu combo personalizado')
+        print('Ha elegido Armar tu combo personalizado')
         mostrar_menu()
         
     elif opcion == '10':
         print('Muchas gracias por su visita!')
-        break
+        eleccion = False
     else: 
         print('Por favor, ingrese un número de opción valido.')
         continue
+    
+    if eleccion:
+        while True:
+            repetir = input('¿Desea agregar algo mas al carrito? (SI/NO): ').lower()
+            print()
+            if repetir == 'si':
+                break
+            elif repetir == 'no':
+                eleccion = False
+                break
+            else:
+                print('Ingrese una opcion valida')
+    
+print('Estamos preparando su pedido.')
+print('Incluye los siguientes elementos:')
+for comida, precio in carrito_de_compras.items():
+    print(f'{comida}: {precio}')
+print()
 
-    repetir = input('¿Desea agregar algo mas al carrito? (SI/NO): ').lower()
-    if repetir != 'si':
-        break
-
-print('Su pedido es; ')
-print(carrito_de_compras)
-
-# Cambios3
+carrito = list(carrito_de_compras.values())
+monto_total = sum(carrito)
+print(f'Monto total de su pedido: {monto_total}')
+print()
+print('¡Gracias por elegirnos y disfrute de su comida!')
